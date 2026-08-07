b3898528e42ec3b166607258888660aa9ce69e86
Yudi454 <lucasyudi445@gmail.com>
2026-08-07 12:40:27 -0300

    Se creo el header

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-1000">
+      <Header />
+    </div>
+  );
+}
