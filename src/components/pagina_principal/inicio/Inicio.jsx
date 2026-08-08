9405443466a70cdadc7712e461817c970509999b
Yudi454 <lucasyudi445@gmail.com>
2026-08-08 17:28:05 -0300

    Se creo la seccion de informacion

--- src/components/pagina_principal/inicio/Inicio.jsx
+++ src/components/pagina_principal/inicio/Inicio.jsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 import React from "react";
 import { Hero } from "./Hero";
+import { Informacion } from "./Informacion";
 
 export const Inicio = () => {
   return (
     <>
       <Hero />
+      <Informacion />
     </>
   );
 };
