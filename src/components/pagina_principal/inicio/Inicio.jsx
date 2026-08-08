be548ded93264eba61aa417fdb6a1af6a41e7d0f
Yudi454 <lucasyudi445@gmail.com>
2026-08-08 19:00:50 -0300

    Se agrego que es la Urologia

--- src/components/pagina_principal/inicio/Inicio.jsx
+++ src/components/pagina_principal/inicio/Inicio.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Hero } from "./Hero";
 import { Informacion } from "./Informacion";
 import { PorqueElegirme } from "./PorqueElegirme";
+import { QueEsUrologia } from "./QueEsUrologia";
 
 export const Inicio = () => {
   return (
@@ -10,6 +11,7 @@ export const Inicio = () => {
       <Hero />
       <Informacion />
       <PorqueElegirme />
+      <QueEsUrologia />
     </>
   );
 };
