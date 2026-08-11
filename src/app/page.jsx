646c71106d473ad7a3e9ebaaee1c4555b0978aa8
Yudi454 <lucasyudi445@gmail.com>
2026-08-11 19:42:53 -0300

    Se realizo especialidades y tratamientos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
-import Image from "next/image";
+import { ServiciosTratamientos } from "@/components/pagina_principal/servicios_tratamientos/ServiciosTratamientos";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ServiciosTratamientos />
       <Footer />
     </>
   );
