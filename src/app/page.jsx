6244c45742c5b693d11941ed9afc9d69172000be
Yudi454 <lucasyudi445@gmail.com>
2026-08-12 16:22:17 -0300

    Se agrego testimonios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
+import { Testimonios } from "@/components/pagina_principal/testimonios/Testimonios";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Testimonios />
       <Footer />
     </>
   );
