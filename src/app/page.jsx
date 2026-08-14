b05cc863e306d59d2cc717553b8855d53fd5b5a9
Yudi454 <lucasyudi445@gmail.com>
2026-08-14 13:35:55 -0300

    Se creo consultorios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
+import { Consultorios } from "@/components/pagina_principal/consultorios/Consultorios";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Consultorios />
       <Footer />
     </>
   );
