f1ad56fe777ac064542c754bcd71596a5f73ea29
Yudi454 <lucasyudi445@gmail.com>
2026-08-29 19:01:47 -0300

    Se agrego animacion de scroll

--- src/components/boton_volver/BotonVolver.jsx
+++ src/components/boton_volver/BotonVolver.jsx
@@ -1,23 +1,19 @@
 "use client";
 
+import { useStore } from "@/store/useStore";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { animate, motion } from "framer-motion";
 
 export const BotonVolver = () => {
+  const scrollbar = useStore((state) => state.scrollbar);
   return (
     <div>
       <motion.button
         onClick={() => {
-          animate(window.scrollY, 0, {
-            duration: 0.8,
-            ease: "easeInOut",
-            onUpdate: (value) => {
-              window.scrollTo(0, value);
-            },
-          });
+          scrollbar?.scrollTo(0, 0, 800);
         }}
-        className="fixed bottom-6 right-6 z-40 flex h-12 w-12 flex-col items-center justify-center rounded-full bg-[#0C71C3] text-white shadow-lg transition-all duration-300 hover:scale-110 hover:bg-[#095b9e]"
+        className="fixed bottom-6 right-6 z-0 flex h-12 w-12 flex-col items-center justify-center rounded-full bg-[#0C71C3] text-white shadow-lg transition-all duration-300 hover:scale-110 hover:bg-[#095b9e]"
         aria-label="Volver arriba"
       >
         <span className="text-lg leading-2">
