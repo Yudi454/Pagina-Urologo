fb78ccfe010b11e0d651238196098526f7b937db
Yudi454 <lucasyudi445@gmail.com>
2026-08-31 15:40:37 -0300

    Se esta tratando de reducir la demora al cargar el video

--- src/components/pagina_principal/inicio/Informacion.jsx
+++ src/components/pagina_principal/inicio/Informacion.jsx
@@ -49,6 +49,7 @@ export const Informacion = () => {
             autoPlay
             loop
             playsInline
+            preload="metadata"
           >
             <source src="/video1.mp4" type="video/mp4" />
           </video>
